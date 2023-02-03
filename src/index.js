f8e0a2d561a60afd9b959dff7e1592285b546d6b
Alexandra <alexandra@192.168.1.50>
2023-02-03 14:39:20 +0300

    start: Morse code decoder

--- src/index.js
+++ src/index.js
@@ -38,8 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrSymbol = [];
+    let string = '';
+    let num = 0;
+
+    for (let key of expr) {
+        string += key;
+        num++;
+        if (num === 10) {
+            arrSymbol.push(string)
+            string = '';
+            num = 0;
+        }
+    }
+    return arrSymbol.map(elem => {
+        let symbol = '';
+        if (elem === '**********') {
+            symbol = ' ';
+            return symbol;
+        }
+        for (let i = 0; i <= elem.length; i = i + 2) {
+            if (elem[i] === '1' && elem[i + 1] === '1') symbol += '-';
+            if (elem[i] === '1' && elem[i + 1] === '0') symbol += '.';
+        }
+        return symbol;
+    }).map(symbol => {
+        if (symbol === ' ') return ' ';
+        if (symbol in MORSE_TABLE) {
+            return MORSE_TABLE[symbol]
+        }
+    }).join('');
 }
+
 
 module.exports = {
     decode
